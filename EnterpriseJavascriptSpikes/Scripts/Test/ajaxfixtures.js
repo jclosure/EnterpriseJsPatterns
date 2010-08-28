da634ca2fa46aa5de89577b8590f3f04bfe35525
jclosure <jclosure@gmail.com>
2010-08-28 01:03:34 -0500

    Added AjaxFixtures
    Abstracts mocking implementations away from consumer.  Mocks fixtures provide
    a repository of urls and json literals that they return.

---
+++ EnterpriseJavascriptSpikes/Scripts/Test/ajaxfixtures.js
@@ -0,0 +1,28 @@
+﻿
+var AjaxFixtures = function () {
+
+    options = {
+        dataType: 'json'
+    };
+
+    return {
+        "/some/json": function (options) {
+
+            var opts = $.extend({}, options, {
+                url: '/some/json',
+                dataType: 'json'
+            });
+
+            $.mockjax(opts);
+        },
+        "mock": function (key, options) {
+            var opts = $.extend({}, this.options, options);
+            //opts
+            this[key](opts);
+        },
+        "init": function () {
+            this['/some/json']();
+            return this;
+        }
+    };
+};
