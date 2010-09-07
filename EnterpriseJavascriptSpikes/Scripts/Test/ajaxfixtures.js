cada1b5ab4d4eab0d4a3aa6ac322816bc250e86a
jclosure <jclosure@gmail.com>
2010-09-06 22:58:21 -0500

    Refactored Test fixture pattern

--- EnterpriseJavascriptSpikes/Scripts/Test/ajaxfixtures.js
+++ EnterpriseJavascriptSpikes/Scripts/Test/ajaxfixtures.js
@@ -6,22 +6,34 @@ var AjaxFixtures = function () {
     };
 
     return {
-        "/some/json": function (options) {
+        "template": function (options) {
 
-            var opts = $.extend({}, options, {
-                url: '/some/json',
-                dataType: 'json'
-            });
+            
 
-            $.mockjax(opts);
+            $.mockjax(options);
+            return this;
         },
         "mock": function (key, options) {
             var opts = $.extend({}, this.options, options);
             //opts
-            this[key](opts);
+            if (!!!opts.url)
+                opts.url = key;
+            //store the executed method - fluent
+            this[key] = this.template(opts);
+            //this[key](opts);
+            return this;
         },
+        //custon section begin
+        "some/other/url?message=foo": function (options) {
+            var opts = $.extend({}, options, {
+                url: 'some/other/url?message=foo'
+            });
+
+            $.mockjax(options);
+        },
+        //custom section end
         "init": function () {
-            this['/some/json']();
+            //this['/some/json']();
             return this;
         }
     };
